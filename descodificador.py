1c134934c0cd11127006a41b00325e971f267cc0
Andre Araujo <andredearaujo2007@gmail.com>
2025-08-26 00:48:42 +0100

    create the new file

---
+++ descodificador.py
@@ -0,0 +1,20 @@
+# This application it will Decrpyt letter in game murder
+# developed by Andre de Araujo
+# Date 25/08/2025
+
+from string import ascii_lowercase, ascii_uppercase
+
+# Build the mapping dictionary
+# (a<-->Z, b<-> Y, ..., z<-> A and A<-z>, ..., Z<->a)
+pairs = list(zip(ascii_lowercase, ascii_uppercase[::-1]))
+letters = {lc: uc for lc, uc in pairs}
+letters.update({uc: lc for lc, uc in pairs})
+
+
+def transform(text: str) -> str:
+    return ''.join(letters.get(ch, ch)for ch in text)
+
+
+# Example usage
+user_input = input("Type your sequence: ")
+print("The answer will be: ", transform(user_input))
