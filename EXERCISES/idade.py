32af74df4ee333e0ce5e4c098549be580183ce86
Andre Araujo <andredearaujo2007@gmail.com>
2025-10-09 11:36:28 +0100

    Adding file of exercices idade, media and imc

---
+++ EXERCISES/idade.py
@@ -0,0 +1,7 @@
+# Generate Age and Name
+# . strip this funcion wiil remove all whitespace
+name = input("Type your name: ").strip()
+age = input("Type your age: ")
+
+# Result your name and age
+print(f"Your name is: {name} and you have {age} years old")
---
+++ EXERCISES/media.py
@@ -0,0 +1,11 @@
+# Programa que faz a soma de 3 numeros
+# No final sai a media aritimetica deles
+
+a = float(input("Type the first number: "))
+b = float(input("Type the second number: "))
+c = float(input("Type the third number: "))
+
+result = a + b + c
+avarage = result / 3
+
+print(f"The avarage of three numbers will be {avarage:.2f} ")
