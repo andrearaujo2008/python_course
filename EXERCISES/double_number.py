a9cc625ebb19d9498c7e14094460fd26ab9e253b
Andre Araujo <andredearaujo2007@gmail.com>
2025-10-14 19:55:23 +0100

    Exercises done in October 14th 2025

---
+++ EXERCISES/double_number.py
@@ -0,0 +1,12 @@
+# This app will sum the double from three numbers
+
+# Read the numbers from user
+n1 = float(input("Type the first number: "))
+n2 = float(input("Type the second number: "))
+n3 = float(input("Type the third number: "))
+
+# This sum from numbers double
+sum = (n1 * 2) + (n2 * 2) + (n3 * 2)
+
+# This is result
+print(f"The result double from three numbers are: {sum:.2f}")
