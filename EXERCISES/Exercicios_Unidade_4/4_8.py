bde40889dd385b8eeb8713d78ebcea25c84fa11f
Andre Araujo <andredearaujo2007@gmail.com>
2026-01-07 10:11:43 +0000

    Finalized part 1 4.8

--- EXERCISES/Exercicios_Unidade_4/4_8.py
+++ EXERCISES/Exercicios_Unidade_4/4_8.py
@@ -1,7 +1,18 @@
 # Program will display 5 words per line
 
-frase = input("Insert at least 5 words: ")
-palavras = frase.split()
-qtd = len(palavras)
+qtd = 0
+
+while qtd < 5:
+
+    # Here you insert the phrase
+    frase = input("Insert at least 5 words: ")
+
+    palavras = frase.split()  # Here the words will be separated
+
+    qtd = len(palavras)  # Count how many words exist on the phrase
+
+    if qtd < 5:
+
+        print("Your phrase must have at least 5 words. Please write again!")
 
 print(f"A quantidade de palavras e: {qtd}")
