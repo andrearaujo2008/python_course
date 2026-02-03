f09e076f183f72b5a59157342973e92c81d835db
Andre Araujo <andredearaujo2007@gmail.com>
2026-02-03 07:35:31 +0000

    Estudos da sobre funções unidade 5 do livro

---
+++ Unidade_5_Funcoes/retun.py
@@ -0,0 +1,9 @@
+# Criando uma funcao
+def dobrar(numero):
+    total = numero * 2  # Somando a funcao multiplicado por 2
+    return total  # Resultado da funcao que saida vai ser 200
+
+
+resultado = dobrar(100) * 2  # Recebendo do return o valor 200 que vai ser multiplicado por 2
+
+print(resultado)  # Saida será 400
