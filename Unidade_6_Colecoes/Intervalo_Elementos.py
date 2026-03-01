a50a963b8bf25808d56a2b6a5a028959be6787e6
Andre Araujo <andredearaujo2007@gmail.com>
2026-03-01 22:05:58 +0000

    Chapter 6 Collection 01_03_2026

---
+++ Unidade_6_Colecoes/Intervalo_Elementos.py
@@ -0,0 +1,9 @@
+# Acessando um intervalo de elementos em uma lista
+lista = [1, 2, 3, 4, 5]
+print(lista[0:2])  # Saida [1,2,3]
+print(lista[1:4])  # Saida [2,3,4]
+
+# Acessando um intervalo de caracteres em uma string
+frase = "Ola python!"
+print(frase[0:2])  # Saida vai ser: Ola
+print(frase[4:10])  # Saida vai ser: Python
---
+++ Unidade_6_Colecoes/Unico_Elemento.py
@@ -0,0 +1,9 @@
+# Acessando um elemento unico em uma lista
+lista = [1, 2, 3, 4, 5]
+print(lista[0])  # Saida 1
+print(lista[2])  # Saida 3
+
+# Acessando um caractere unico em um string
+frase = "Ola python!"
+print(frase[0])  # Saida vai ser: O
+print(frase[-1])  # Saida vai ser: !
