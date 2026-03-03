db42e86688a1c70ec22b1adc438c2150c141fd11
dede <andredearaujo2007@gmail.com>
2026-03-03 17:09:37 +0000

    Collection_03_03_2026

---
+++ Unidade_6_Colecoes/final_colecao.py
@@ -0,0 +1,9 @@
+ #Acessando um elemento a partir do final uma lista
+lista = [1, 2, 3, 4, 5]
+print(lista[-1])  # Saida 5
+print(lista[-2])  # Saida 4
+
+# Acessando um caractere a partir do final de um string
+frase = "Ola python!"
+print(frase[-1])  # Saida vai ser: !
+print(frase[-2])  # Saida vai ser: n
