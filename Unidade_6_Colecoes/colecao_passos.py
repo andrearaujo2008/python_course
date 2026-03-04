1200bd84579c9dd51da1aff67c3efdf813e0a7e7
dede <andredearaujo2007@gmail.com>
2026-03-04 18:02:53 +0000

    Exercicios feito no dia 04_03_2026

---
+++ Unidade_6_Colecoes/colecao_passos.py
@@ -0,0 +1,7 @@
+#Acessando elementos de uma lista usando passos 2
+lista = [1, 2, 3, 4, 5]
+print(lista[0:5:2])  # Saida 1, 3, 5
+
+# Acessando um caractere de um string com passos 2
+frase = "Ola python!"
+print(frase[0:12:2])  # Saida vai ser: Oapto!
---
+++ Unidade_6_Colecoes/invertido_colecao_passo.py
@@ -0,0 +1,7 @@
+#Acessando os elementos de uma lista de forma invertida
+lista = [1, 2, 3, 4, 5]
+print(lista[::-1])  # Saida [5, 4, 3, 2, 1]
+
+# Acessando um caractere de um string de forma invertida
+frase = "Ola python!"
+print(frase[::-1])  # Saida vai ser: !nohtyp alO
