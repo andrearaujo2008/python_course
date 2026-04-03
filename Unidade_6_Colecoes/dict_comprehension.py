95430bc2f9aec3097377772177ef819e08b66c4c
Andre Araujo <andredearaujo2007@gmail.com>
2026-04-03 18:09:12 +0100

    exercices done about Unit 6

---
+++ Unidade_6_Colecoes/dict_comprehension.py
@@ -0,0 +1,5 @@
+# Criando um dict comprehension
+
+numeros = [1, 2, 3, 4, 5]
+quadrados = {x: x**2 for x in numeros}
+print(quadrados)  # Saida {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
--- Unidade_6_Colecoes/set_comprehension.py
+++ Unidade_6_Colecoes/set_comprehension.py
@@ -3,4 +3,4 @@ numeros = [1, 2, 3, 2, -5, 4, 3, 1, -2]
 
 quadrados_unicos = {x**2 for x in numeros if x > 0}
 
-print(quadrados_unicos)
+print(quadrados_unicos)  # Saida {16, 1, 4, 9}
